c633744e439f59d1be215d6ff9d156ae56b0b8f4
Jhonata Souza <einasota@gmail.com>
2021-05-01 15:50:43 -0300

    Enviando Atualizações

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,12 +1,21 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+type Episode = {
+    id: string;
+    title: string;
+    members: string;
+}
+type HomeProps = {
+    episodes: Episode[];
+}
+export default function Home(props: HomeProps) {
 
     return <>
         <p>{JSON.stringify(props.episodes)}</p>
     </>;
 }
 
-export async function getStaticProps() {
-    const response = await fetch('http://localhost:3333/episodes')
+export const getStaticProps: GetStaticProps = async () => {
+    const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc')
     const data = await response.json()
 
     return {
